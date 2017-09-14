c17a5f8b0078fbf405705ecbdb6f9dec09e8d557
Shashank Shetye Saudagar <shashank@ajency.in>
2017-09-14 19:17:14 +0530

    Migrations

---
+++ src/migrations/2017_09_14_182448_create_mapping_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMappingTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('fileuploads_mapping', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('object_model');
+            $table->integer('object_id');
+            $table->string('file_model');
+            $table->integer('file_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('fileuploads_mapping');
+    }
+}
