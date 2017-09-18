b3f488fcdc383db15eea10695a9c84adcd61019a
Shashank Shetye Saudagar <shashank@ajency.in>
2017-09-18 17:26:47 +0530

    Delete Everything

--- src/config/ajfileupload.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-return [
-
-	"sizes" => ["thumb" => ['height' => 56,  "width" => 100],],
-	"watermark" => ['path' => public_path().'/img/facebook.png', 'position'=>'bottom-right', 'x'=> 10, 'y'=>10],
-];
