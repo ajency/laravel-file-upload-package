42898f92296961ad2f77cd308d9655e5c8ee0b3c
Shashank Shetye Saudagar <shashank@ajency.in>
2017-09-20 13:11:25 +0530

    Files Methods

--- src/FileUpload.php
+++ src/FileUpload.php
@@ -29,7 +29,7 @@ trait FileUpload{
         if($upload->upload($image,$this,self::class,$is_watermarked,$is_public)){
      	   return $upload->id;
     	}else{
-    		return false
+    		return false;
     	}
 	}
 	public function mapImage($image_id){
@@ -43,7 +43,7 @@ trait FileUpload{
 	}
 	public function getImages(){
 		$uploads = array();
-		$images = $this->media()->pluck('id')->toArray();
+		$images = $this->media()->where('file_type',FileUpload_Photos::class)->pluck('id')->toArray();
 		$images = FileUpload_Mapping::whereIn('id',$images)->get();
 		foreach ($images as $image) {
 			$uploads[$image->file_id] = array('id'=>$image->file_id);
@@ -51,6 +51,42 @@ trait FileUpload{
 			foreach ($varients as $varient) {
 				$uploads[$image->file_id][$varient->size]=$varient->url;
 			}
+		}
+		return $uploads;
+	}
+
+
+
+
+	public function uploadFile($file,$is_public=true,$name=""){
+		if($this->validateFile($file,1)) return false;
+		$upload = new FileUpload_Files;
+        $upload->name = $name;
+        $upload->slug = str_slug($name);
+        $upload->is_public = $is_public;
+        $upload->save();
+        if($upload->upload($image,$this,self::class,$is_public)){
+     	   return $upload->id;
+    	}else{
+    		return false;
+    	}
+	}
+	public function mapFile($file_id){
+		$check = FileUpload_Mapping::where('file_id',$file_id)->where('file_type',FileUpload_Files::class)->count();
+		if ($check>0) return false;
+		$file = FileUpload_Files::find($file_id);
+        $mapping = new FileUpload_Mapping;
+        $this->media()->save($mapping);
+        $file->mapping()->save($mapping);
+        return true;
+	}
+	public function getFiles(){
+		$uploads = array();
+		$files = $this->media()->where('file_type',FileUpload_Files::class)->pluck('id')->toArray();
+		$files = FileUpload_Files::whereIn('id',$files)->get();
+		foreach ($files as $file) {
+			$uploads[$file->id] = array('id'=>$file->id);
+			$uploads[$file->id]['url'] = $file->url; 
 		}
 		return $uploads;
 	}
--- src/models/FileUpload_Files.php
+++ src/models/FileUpload_Files.php
@@ -10,5 +10,31 @@ class FileUpload_Files extends Model
     public function mapping(){
         return $this->morphMany( 'Ajency\FileUpload\models\FileUpload_Mapping', 'file');
     }
-    
+    public function upload($file, $obj_instance, $obj_class, $public){
+    	$config        = config('ajfileupload');
+    	$name= $this->slug.'-'.time().'.'.$file->getClientOriginalExtension();
+    	$disk          = \Storage::disk($config['disk_name']);
+        $ext           = $file->getClientOriginalExtension();
+        if (isset($config['model'][$obj_class])) {
+            $filepath = $config['base_root_path'] . $config['model'][$obj_class]['base_path'].'/'.$obj_instance[$config['model'][$obj_class]['slug_column']]. '/files/' .$obj_instance[$config['model'][$obj_class]['slug_column']].'-'. $name ;
+        } else {
+            $filepath = $config['default_base_path'] . 'files/' . $name 
+        }
+        if ($public) {
+	    	if ($disk->put($fp, $new_img->__toString(), 'public')) {
+	            $this->save();
+	        } else {
+	            return false;
+	        }
+	    } else {
+	        if ($disk->put($fp, $new_img->__toString(), 'private')) {
+	            $this->save();
+	        } else {
+	            return false;
+	        }
+	    }
+	    $this->url = $disk->url($fp);
+        $this->save();
+        return true;
+    }
 }
