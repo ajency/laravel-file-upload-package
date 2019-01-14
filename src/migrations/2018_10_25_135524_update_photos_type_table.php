8e8554f62a0e193182505fb72c73b0bc006683b8
Shashank Shetye Saudagar <shashank@ajency.in>
2019-01-14 16:50:22 +0530

    Create 2018_10_25_135524_update_photos_type_table.php

---
+++ src/migrations/2018_10_25_135524_update_photos_type_table.php
@@ -0,0 +1,29 @@
+<?php
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+class UpdatePhotosTypeTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('fileupload_photos', function (Blueprint $table) {
+            $table->string('type');
+        });
+    }
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('fileupload_photos', function (Blueprint $table) {
+            $table->dropColumn('type');
+        });
+    }
+}
